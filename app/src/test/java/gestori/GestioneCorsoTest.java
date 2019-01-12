9791aaf722919c5f09b9e7e961b19a127c19adaa
mikeleruo <mikeleruo@outlook.it>
2019-01-12 16:02:41 +0100

    Unit Test for "GestioneCorso" : verificaCodiceCorso()

--- app/src/test/java/gestori/GestioneCorsoTest.java
+++ app/src/test/java/gestori/GestioneCorsoTest.java
@@ -25,29 +25,54 @@ public class GestioneCorsoTest {
 
     private DatabaseHelper databaseHelper;
     private Context context;
-
+    private  GestoreRightChoice gestoreRightChoice;
+    private Corso corso,corso1;
     @Before
     public void setUp() throws Exception {
         context = getInstrumentation().getContext();
         databaseHelper = new DatabaseHelper(context);
+        corso = new Corso("test", "nome", "docente", "descrizione", "link");
+        corso1 = new Corso("test1", "nome", "docente", "descrizione", "link");
+        gestoreRightChoice = new GestoreRightChoice(context);
     }
 
     @Test
     public void inserisciCorso() {
-        Corso corso = new Corso("codice", "nome", "docente", "descrizione", "link");
-        GestoreRightChoice gestoreRightChoice = new GestoreRightChoice(context);
         gestoreRightChoice.inserisciCorso(corso);
         ArrayList<String> listaNomiCorsi = gestoreRightChoice.getNomeCorsi();
-        assertThat(listaNomiCorsi, hasItems("nome"));
 
+        /* Caso in cui il corso è stato correttamente inserito */
+        assertThat(listaNomiCorsi, hasItems("nome"));
     }
 
     @Test
     public void listaCorsi(){
-        Corso corso = new Corso("codicss", "esempio", "ferrucci", "descrizione", "link");
-        GestoreRightChoice gestoreRightChoice = new GestoreRightChoice(context);
+        /* Creo una lista di corsi vuota dove non aggiungo corsi */
+        ArrayList<Corso> listavuota = gestoreRightChoice.listaCorsi();
+
+        /* Aggiungo un corso richiamando il metodo */
         gestoreRightChoice.inserisciCorso(corso);
+
+        /* Creo una lista di corsi aggiornata con il corso inserito precedentemente */
         ArrayList<Corso> lista = gestoreRightChoice.listaCorsi();
+
+        /* Caso in cui il database ritorna una lista vuota */
+        assertTrue(listavuota.isEmpty());
+
+        /* Caso in cui il database ritorna una lista piena */
         assertFalse(lista.isEmpty());
     }
+
+    @Test
+    public void verificaCodiceCorso(){
+        /* Inserisco "corso1" nella lista dei corsi*/
+        gestoreRightChoice.inserisciCorso(corso1);
+
+        /* Caso il cui il codice del corso non è già presente nel database */
+        assertTrue(gestoreRightChoice.verificaCodiceCorso(corso));
+
+        /* Caso il cui il codice del corso è già presente nel database */
+        assertFalse(gestoreRightChoice.verificaCodiceCorso(corso1));
+    }
+
 }
